65df2627e6d5088510604f101574f70064034517
Vikash Kumar <sharma.vikashkr@gmail.com>
2021-12-09 15:58:46 +0530

    BulkMailerVB

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BulkMailer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(BulkMailer))
+        Me.WorkFolderLabel = New System.Windows.Forms.Label()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.WorkFolderPicker = New System.Windows.Forms.FolderBrowserDialog()
+        Me.WorkFolderPath = New System.Windows.Forms.TextBox()
+        Me.WorkFolderDialogOpener = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WorkFolderLabel
+        '
+        Me.WorkFolderLabel.AutoSize = True
+        Me.WorkFolderLabel.Location = New System.Drawing.Point(29, 38)
+        Me.WorkFolderLabel.Name = "WorkFolderLabel"
+        Me.WorkFolderLabel.Size = New System.Drawing.Size(89, 17)
+        Me.WorkFolderLabel.TabIndex = 1
+        Me.WorkFolderLabel.Text = "Work Folder:"
+        '
+        'Start
+        '
+        Me.Start.BackColor = System.Drawing.Color.LimeGreen
+        Me.Start.Location = New System.Drawing.Point(32, 159)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(86, 33)
+        Me.Start.TabIndex = 2
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = False
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.ForeColor = System.Drawing.Color.Red
+        Me.StatusLabel.Location = New System.Drawing.Point(29, 123)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(48, 17)
+        Me.StatusLabel.TabIndex = 4
+        Me.StatusLabel.Text = "Status"
+        '
+        'WorkFolderPicker
+        '
+        Me.WorkFolderPicker.ShowNewFolderButton = False
+        '
+        'WorkFolderPath
+        '
+        Me.WorkFolderPath.Cursor = System.Windows.Forms.Cursors.Default
+        Me.WorkFolderPath.Location = New System.Drawing.Point(32, 77)
+        Me.WorkFolderPath.Name = "WorkFolderPath"
+        Me.WorkFolderPath.Size = New System.Drawing.Size(464, 22)
+        Me.WorkFolderPath.TabIndex = 5
+        '
+        'WorkFolderDialogOpener
+        '
+        Me.WorkFolderDialogOpener.Location = New System.Drawing.Point(421, 77)
+        Me.WorkFolderDialogOpener.Name = "WorkFolderDialogOpener"
+        Me.WorkFolderDialogOpener.Size = New System.Drawing.Size(75, 23)
+        Me.WorkFolderDialogOpener.TabIndex = 6
+        Me.WorkFolderDialogOpener.Text = "Select"
+        Me.WorkFolderDialogOpener.UseVisualStyleBackColor = True
+        '
+        'BulkMailer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(529, 231)
+        Me.Controls.Add(Me.WorkFolderDialogOpener)
+        Me.Controls.Add(Me.WorkFolderPath)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.WorkFolderLabel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "BulkMailer"
+        Me.Text = "BulkMailer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WorkFolderLabel As Label
+    Friend WithEvents Start As Button
+    Friend WithEvents StatusLabel As Label
+    Friend WithEvents WorkFolderPicker As FolderBrowserDialog
+    Friend WithEvents WorkFolderPath As TextBox
+    Friend WithEvents WorkFolderDialogOpener As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.IO
+
+Public Class BulkMailer
+    Private Sub BulkMailer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+
+        If WorkFolderPath.Text = "" Then
+            StatusLabel.Text = "Please select the Work Folder"
+            Return
+        End If
+        StatusLabel.Text = "Selected Work Folder: " + WorkFolderPath.Text
+
+        Dim DT As DateTime = DateTime.Now
+        Dim logFile As System.IO.StreamWriter
+
+        logFile = My.Computer.FileSystem.OpenTextFileWriter(WorkFolderPath.Text + "\logs.txt", True)
+        logFile.WriteLine(DT.ToString)
+        logFile.WriteLine("Selected Work Folder: " + WorkFolderPath.Text)
+
+        Dim APP As New Excel.Application
+        Dim workbook As Excel.Workbook
+        Dim worksheet As Excel.Worksheet
+
+        Try
+            workbook = APP.Workbooks.Open(WorkFolderPath.Text + "\Distributors.xlsx")
+            worksheet = workbook.Worksheets("sheet1")
+
+            Dim OutlookApp
+            OutlookApp = CreateObject("outlook.Application")
+
+            Dim Mail, DistributorCode
+            Dim I As Integer
+            Dim last_row As Integer
+
+            last_row = APP.WorksheetFunction.CountA(worksheet.Range("A:A"))
+
+            For I = 2 To last_row
+                Mail = OutlookApp.createitem(0)
+                DistributorCode = worksheet.Range("a" & I).Value
+                Mail.TO = worksheet.Range("b" & I).Value
+                Mail.CC = worksheet.Range("c" & I).Value
+                Mail.Subject = worksheet.Range("d" & I).Value
+                Mail.BODY = worksheet.Range("e" & I).Value
+
+                StatusLabel.Text = "Processing Mail for Distributor: " + DistributorCode.ToString
+                logFile.WriteLine("Processing Mail for Distributor: " + DistributorCode.ToString)
+
+                Dim strFileSize As String = ""
+                Dim di As New IO.DirectoryInfo(WorkFolderPath.Text)
+                Dim aryFi As IO.FileInfo() = di.GetFiles("*.*")
+                Dim fi As IO.FileInfo
+
+                For Each fi In aryFi
+                    strFileSize = (Math.Round(fi.Length / 1024)).ToString()
+                    Console.WriteLine("File Name: {0}", fi.Name)
+                    Console.WriteLine("File Full Name: {0}", fi.FullName)
+                    Console.WriteLine("File Size (KB): {0}", strFileSize)
+                    If fi.Name.StartsWith(DistributorCode.ToString + "_") Then
+                        StatusLabel.Text = "Adding Attachment: " + fi.FullName + "for Distributor: " + DistributorCode.ToString
+                        logFile.WriteLine("Adding Attachment: " + fi.FullName + "for Distributor: " + DistributorCode.ToString)
+                        Mail.Attachments.Add(fi.FullName)
+                    End If
+                Next
+
+                StatusLabel.Text = "Sending mail for distributor: " + DistributorCode.ToString
+                logFile.WriteLine("Sending mail for distributor: " + DistributorCode.ToString)
+
+                Mail.send
+
+                StatusLabel.Text = "Delay 5 sec"
+                logFile.WriteLine("Delay 5 sec")
+
+                Threading.Thread.Sleep(5000)
+                logFile.WriteLine("Delay over")
+            Next
+            logFile.WriteLine("Finished")
+        Catch ex As Exception
+            StatusLabel.Text = "Error Occurred: " + ex.Message
+            logFile.WriteLine("Error Occurred: " + ex.Message)
+        Finally
+            workbook.Close()
+            logFile.Close()
+        End Try
+    End Sub
+
+    Private Sub WorkFolderDialogOpener_Click(sender As Object, e As EventArgs) Handles WorkFolderDialogOpener.Click
+        If WorkFolderPicker.ShowDialog() = DialogResult.OK Then
+            WorkFolderPath.Text = WorkFolderPicker.SelectedPath
+        End If
+    End Sub
+
+    Private Sub WorkFolderPath_Cick(sender As Object, e As EventArgs) Handles WorkFolderPath.Click
+        WorkFolderDialogOpener_Click(sender, e)
+    End Sub
+End Class
